322d8b47342cf1ac12534eeb7c0cd132d4ac7da0
RohanDeySarkar <41055662+RohanDeySarkar@users.noreply.github.com>
2020-01-24 11:36:26 +0530

    Add files via upload

---
+++ util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyDyOiG4-cTbVZ_Pkb5_qzvZx1oTpjVsyI8",
+    authDomain: "socialape-a66d6.firebaseapp.com",
+    databaseURL: "https://socialape-a66d6.firebaseio.com",
+    projectId: "socialape-a66d6",
+    storageBucket: "socialape-a66d6.appspot.com",
+    messagingSenderId: "975158247145",
+    appId: "1:975158247145:web:8484b715a7bdc4661b3a78",
+    measurementId: "G-ZKHSKEDGTM"
+};
